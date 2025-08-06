ea88217f97e58cc21132b202e4ff5714c45b9340
siva-narayana-1 <sivanarayanamadhala@gmail.com>
2025-08-06 14:38:06 +0530

    token_generator is updated after error-1

--- token_generator.py
+++ token_generator.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-import os
 import random
 import json
 from utils import load_fs
+import os
 file_name = "token.json"
 token = {}
 def load_token():
@@ -47,38 +46,3 @@ def remove_locked_user(username,role):
             print(f"{user} is unLocked.")
         else:
             print("user not available.")
-    
-=======
-import os
-import random
-import json
-file_name = "token.json"
-token = {}
-def load_token():
-    global token
-    if os.path.exists(file_name):
-        with open(file_name, "r") as f:
-            token = json.load(f)
-    else:
-        return "{file_name} not found"
-    return token
-def save():
-    with open(file_name, "w") as f:
-        json.dump(token, f, indent=4)
-
-def token_user(username):
-    username = username
-    token_number = username+str(random.randint(0,9))
-    token[username] = token_number
-    save()
-    return token_number
-
-def remove_token(username):
-    if username in token:
-        token.pop(username)
-        save()
-        return "users loged out."
-    else:
-        return "No user found."
-    
->>>>>>> f1f0fbfe736b9e85060971a7d57bce82b3ccfd20
