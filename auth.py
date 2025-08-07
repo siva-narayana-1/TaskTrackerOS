f8670bda536aa744bb2320a82bb0d5b01a929d8a
siva-narayana-1 <sivanarayanamadhala@gmail.com>
2025-08-07 13:26:36 +0530

    commited day 07

--- auth.py
+++ auth.py
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import sys
-import json
-import os
-from token_generator import token_user, load_token
-from utils import load_fs, save_fs
-
-fs = load_fs()
-def signup():
-    try:
-        username = input("Choose a username: ")
-        password = input("Enter a password: ")
-        role = input("Enter your role (admin/user): ")
-
-        if role in fs:
-            if username in fs[role]:
-                print(f"{username} is already taken.")
-                return None
-            else:
-                fs[role][username] = password
-        else:
-            fs[role] = {username: password}
-
-        save_fs()
-        print(f"{username} registered successfully as {role} ✅")
-        print("Logging you in automatically...\n")
-        token = token_user(username)
-        return role, username, token
-
-    except Exception:
-        exc_type, exc_msg, exc_line = sys.exc_info()
-        print(f"{exc_type} at line {exc_line.tb_lineno} as {exc_msg}")
-        return None
-
-def login():
-    try:
-        username = input("Enter your username: ")
-        password = input("Enter your password: ")
-
-        for role in fs:
-            if username in fs[role]:
-                if fs[role][username] == password:
-                    check_token = load_token()
-                    if username not in check_token:
-                        token = token_user(username)
-                        return role, username,token
-                    else:
-                        token = "No token"
-                        return role, username, token
-                else:
-                    print("Incorrect password.")
-                    return None
-
-        print(f"{username} is incorrect. Please check once.")
-        return None
-
-    except Exception:
-        exc_type, exc_msg, exc_line = sys.exc_info()
-        print(f"{exc_type} at line {exc_line.tb_lineno} as {exc_msg}")
-        return None
-=======
 import sys
 import json
 import os
@@ -126,4 +65,3 @@ def login():
         exc_type, exc_msg, exc_line = sys.exc_info()
         print(f"{exc_type} at line {exc_line.tb_lineno} as {exc_msg}")
         return None
->>>>>>> f1f0fbfe736b9e85060971a7d57bce82b3ccfd20
--- main.py
+++ main.py
@@ -1,90 +1,4 @@
-<<<<<<< HEAD
-from auth import login, signup, load_fs
-from task import add, view
-from token_generator import load_token, remove_token, view_logged, remove_locked_user
 
-check_token = load_token()
-
-def main():
-    load_fs()
-    print("Task Tracker")
-    while True:
-        choice = input("\n1. Login\n2. Signup\n3. Exit\nChoose an option: ")
-        if choice == "1":
-            session = login()
-            if session:
-                check_token = load_token()
-                role, username, token = session
-
-                if role == "admin" and check_token.get(username) == token:
-                    admin_dashboard(role, username, token)
-                elif role == "user" and check_token.get(username) == token:
-                    user_dashboard(role, username, token)
-                elif token == "No token":
-                    print(f"{username} was Locked. Ask Admin to unlock.")
-                    continue 
-                else:
-                    print(f"Unknown role or token mismatch.")
-                break
-            else:
-                print("Login failed")
-
-        elif choice == "2":
-            session = signup()
-            if session:
-                role, username, token = session
-                if role == "admin" and check_token.get(username) == token:
-                    admin_dashboard(role, username, token)
-                elif role == "user" and check_token.get(username) == token:
-                    user_dashboard(role, username, token)
-                else:
-                    print(f"Unknown role or token mismatch.")
-                break
-
-        elif choice == "3":
-            print("Exiting.")
-            break
-
-        else:
-            print("Invalid choice. Try again.")
-
-def admin_dashboard(role, username, token):
-    print(f"\nWelcome to the {role} dashboard.")
-    print(f"Hello, {username}!")
-    while True:
-        check_token = load_token()
-        choice = input("1. View Users\n2. Remove Locked User.\n3. Logout\nChoose an option: ")
-        if choice == "1" and check_token.get(username) == token:
-            view_logged(username)
-        elif choice == "2" and check_token[username] == token:
-            remove_locked_user(username,role)
-        elif choice == "3" and check_token.get(username) == token:
-            remove_token(username)
-            print("Logging out...")
-            break
-        else:
-            print("Invalid choice.")
-
-def user_dashboard(role, username, token):
-    print(f"\nWelcome to the {role} dashboard.")
-    print(f"Hello, {username}!")
-    while True:
-        choice = input("1. Add Task\n2. View Tasks\n3. Logout\nChoose an option: ")
-        check_token = load_token()
-        if choice == "1" and check_token.get(username) == token:
-            add(username)
-        elif choice == "2" and check_token.get(username) == token:
-            view(username)
-        elif choice == "3":
-            remove = remove_token(username)
-            print(f"{remove}")
-            break
-        else:
-            print("Invalid choice.")
-
-if __name__ == "__main__":
-    main()
-=======
 from auth import login, signup, load_fs
 from task import add, view
 from token_generator import load_token, remove_token
@@ -163,4 +77,4 @@ def user_dashboard(role, username, token):
 
 if __name__ == "__main__":
     main()
->>>>>>> f1f0fbfe736b9e85060971a7d57bce82b3ccfd20
+
